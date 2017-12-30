7af129f9e9c3088926f21ecd1973861c176e53d4
Enricds <enric.delgado@gmail.com>
2017-12-30 09:17:55 +0100

    afegit que llegeixi tots els fitxers d’un directori que es passa per la url

--- main.js
+++ main.js
@@ -4,11 +4,24 @@
 var llegirfitxer = require('./llegircsv.js');
 
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.write(req.url)
-  res.end();
+  res.write(req.url);
+
+  fs.readdir(req.url, function(err, items) {
+    //console.log(JSON.stringify(items));
+    //res.write(JSON.stringify(items));
+
+    for (var i=0; i<items.length; i++) {
+              res.write(JSON.stringify(items[i]));
+      //  console.log(JSON.stringify(items[i]));
+    }
+//  res.write('final');
+    res.end();
+  });
+
 });
 
 var inputFile='server1.csv';
